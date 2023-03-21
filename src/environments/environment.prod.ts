782c6c8cc5240fdcb308806f6e65c80a404c0d32
Zohaib Tariq <se.zohaib@gmail.com>
2023-03-22 04:42:51 +0500

    updated an ip...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ const ports = {
   customer: 8080,
   api: 3000,
 };
-const serverIPDomain = '20.25.132.122';
+const serverIPDomain = '172.173.208.174';
 const apiPathWithPort = 'http://' + serverIPDomain + ':' + ports.api + '/';
 export const environment = {
   production: true,
